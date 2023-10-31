ecd1c3876ac4d128337cf2710458b0780b4a7384
rbgautam <rahulbgautam@gmail.com>
2023-10-31 14:17:01 -0500

    Added nodemon Set Header for response

--- httpserver.js
+++ httpserver.js
@@ -3,7 +3,8 @@ const http = require('http');
 const server = http.createServer((req,res)=>{
     const{headers,url,method} =req;
     console.log(headers,url,method);
-    res.write('Hello to Node.js');
+    res.setHeader('content-type','text-plain');
+    res.write('Hello world from Node.js');
     res.end();
 
 });
